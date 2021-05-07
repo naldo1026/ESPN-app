dd2ac69322239c2a251705935c879dcf77ca522e
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-05-07 18:20:14 +0100

    added edit, update and destroy methods to the project and added an edit view file

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,6 +21,27 @@ class MessagesController < ApplicationController
    end
   end
 
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def message_params
