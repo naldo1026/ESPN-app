7cf10a2e5dcf59dba677cdc99b922871e88656e5
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-05-09 16:25:22 +0100

    edit update and destroy added to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,29 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to message_path(@message)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.find(params[:id])
+
+    @comment.destroy
+    redirect_to message_path(@message)
+  end
+
   private
 
   def comment_params
